9d5d3fe5a8513fd22cfc12e15ad2e887eab3308e
Marcus Lee <marcustutorial@hotmail.com>
2022-06-07 15:37:32 +0800

    finished adding telegram subscribe and unsubscribe feature

--- srcs/bot/telegram.js
+++ srcs/bot/telegram.js
@@ -1,15 +1,78 @@
 const { Telegraf } = require('telegraf');
 require('dotenv').config();
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
-bot.start((ctx) => ctx.reply('Welcome to LS Machinery BOT!'));
-bot.command('subscribe', (ctx) => {
-  // add chat_id to database
+const { Client } = require('@notionhq/client');
 
-  ctx.reply('You are subscribed to the notifications! 🎉');
+// declaring constants
+const { ENV } = process.env;
+const SECRET = (ENV === 'dev') ? process.env.NOTION_SECRET_DEV : process.env.NOTION_SECRET;
+const SUBSCRIBER_DB_ID = (ENV === 'dev') ? process.env.NOTION_SUBSCRIBER_DB_ID_DEV : process.env.NOTION_SUBSCRIBER_DB_ID;
+
+// Initializing a client
+const notion = new Client({ auth: SECRET });
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.start((ctx) => ctx.reply('Welcome to LS Machinery BOT!'));
+
+bot.command('subscribe', async (ctx) => {
+  // add chat_id to database
+  try {
+    // find if chat_id exists
+    let resp = await notion.databases.query({
+      database_id: SUBSCRIBER_DB_ID,
+      filter: { property: 'ChatID', rich_text: { equals: ctx.chat.id.toString() } },
+      page_size: 1,
+    });
+
+    // throw error if already exist
+    if (resp.results.length > 0) throw new Error(`${ctx.chat.first_name} already subscribed`);
+
+    // create subscriber with chat_id
+    resp = await notion.pages.create({
+      parent: { database_id: SUBSCRIBER_DB_ID },
+      properties: {
+        Name: { title: [{ text: { content: `${ctx.chat.first_name}${ctx.from.last_name ? ` ${ctx.from.last_name}` : ''}` } }] },
+        Username: { rich_text: [{ text: { content: ctx.chat.username } }] },
+        ChatID: { rich_text: [{ text: { content: ctx.chat.id.toString() } }] },
+      },
+    });
+  } catch (error) {
+    if (error.message.includes('already subscribed')) {
+      ctx.reply('Already subscribed');
+    } else {
+      ctx.reply('Unable to subscribe at the moment');
+    }
+    return;
+  }
+
+  ctx.reply('You are now subscribed to the notifications! 🎉');
 });
-bot.command('unsubscribe', (ctx) => {
+
+bot.command('unsubscribe', async (ctx) => {
   // remove chat_id from database
+  try {
+    // find if chat_id exists
+    const resp = await notion.databases.query({
+      database_id: SUBSCRIBER_DB_ID,
+      filter: { property: 'ChatID', rich_text: { equals: ctx.chat.id.toString() } },
+      page_size: 1,
+    });
+
+    // throw error if not exist
+    if (resp.results.length < 1) throw new Error(`no subscribers found with chat_id of ${ctx.chat.id.toString()}`);
+
+    // get the page_id and archive it
+    const pageId = resp.results[0].id;
+    notion.pages.update({ page_id: pageId, archived: true });
+  } catch (error) {
+    if (error.message.includes('no subscribers found')) {
+      ctx.reply('You are not subscribed');
+    } else {
+      ctx.reply('Unable to unsubscribe, please try again later.');
+    }
+    return;
+  }
 
   ctx.reply('You will stop receiving notifications.');
 });
